264f0feb9e0f3fee6b69e66c53ac3b7bf27f1c22
aleksandrachrustek <106326730+aleksandrachrustek@users.noreply.github.com>
2022-05-30 00:04:39 +0200

    loop for rounds, input from user

--- src/Main.java
+++ src/Main.java
@@ -5,33 +5,35 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Scanner scan = new Scanner(System.in);
-
-        Map map = new Map(5,1,0,2,2,0);
-        map.show();
-
-        scan.nextByte();
-
-        map.update();
-        map.show();
+        int size,count,amountOfMonkeys,amountOfLions,amountOfAdults,amountOfChildren,numberOfHidings;
 
         //wczytanie ilości agentów podanej przez użytkownika
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Podaj rozmiar mapy: ");
+        size = scan.nextInt();
+        System.out.println("Podaj ilość małp: ");
+        amountOfMonkeys = scan.nextInt();
+        System.out.println("Podaj ilość lwów: ");
+        amountOfLions = scan.nextInt();
+        System.out.println("Podaj ilość dorosłych: ");
+        amountOfAdults = scan.nextInt();
+        System.out.println("Podaj ilość dzieci: ");
+        amountOfChildren = scan.nextInt();
+        System.out.println("Podaj ilość kryjówek: ");
+        numberOfHidings = scan.nextInt();
+        System.out.println("Podaj ilość rund do rozegrania: ");
+        count = scan.nextInt();
+        scan.close();
 
-//        System.out.println("Podaj rozmiar mapy: ");
-//        Simulation.boardSize = scan.nextInt();
-//        System.out.println("Podaj ilość małp: ");
-//        Simulation.amountOfMonkeys = scan.nextInt();
-//        System.out.println("Podaj ilość lwów: ");
-//        Simulation.amountOfLions = scan.nextInt();
-//        System.out.println("Podaj ilość dorosłych: ");
-//        Simulation.amountOfAdults = scan.nextInt();
-//        System.out.println("Podaj ilość dzieci: ");
-//        Simulation.amountOfChildren = scan.nextInt();
-//        System.out.println("Podaj ilość tarzanów: ");
-//        Simulation.amountOfTarzans = scan.nextInt();
-//        System.out.println("Podaj ilość kryjówek: ");
-//        Simulation.numberOfHidings = scan.nextInt();
-//        scan.close();
+        //stworzenie mapy o zadanych parametrach
+        Map map = new Map(size,amountOfMonkeys,amountOfLions,amountOfChildren,amountOfAdults,numberOfHidings);
+        map.show();
+        //wyświetlanie zaktualizowanej mapy przez podaną ilość rund
+        for(int i=1;i<=count;i++){
+            System.out.println("Runda "+i+":");
+            map.update();
+            map.show();
+        }
 
     }
 }
