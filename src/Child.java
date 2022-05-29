d858728a5c272fc2887f75a0da82db6367ced775
mandra882 <mandra822@gmail.com>
2022-05-29 21:12:18 +0200

    xxx

--- src/Child.java
+++ src/Child.java
@@ -1,14 +1,7 @@
 public class Child extends Human {
     private int age;
-<<<<<<< HEAD
     Child(int x, int y){
         super(x,y);
     }
 
-=======
-
-    Child(int x, int y) {
-        super(x, y);
-    }
->>>>>>> 77b97321e2f882ea518484bba550922288f5e457
 }
