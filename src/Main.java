132bca00fa60a371c85549fa5f822fecb584af41
aleksandrachrustek <106326730+aleksandrachrustek@users.noreply.github.com>
2022-05-29 16:21:24 +0200

    Creating and displaying a board

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,27 @@
 import java.util.Scanner;
 
 public class Main {
-    int amountOfMonkeys;
-    int amountOfLions;
-    int amountOfAdults;
-    int amountOfChildren;
-    int amountOfTarzans;
-    int numberOfHidings;
-    public void main(String[] args) {
+
+    public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
         System.out.println("Podaj rozmiar mapy: ");
-        Simulation.mapSize = scan.nextInt();
+        Simulation.boardSize = scan.nextInt();
         System.out.println("Podaj ilość małp: ");
-        amountOfMonkeys = scan.nextInt();
+        Simulation.amountOfMonkeys = scan.nextInt();
         System.out.println("Podaj ilość lwów: ");
-        amountOfLions = scan.nextInt();
+        Simulation.amountOfLions = scan.nextInt();
         System.out.println("Podaj ilość dorosłych: ");
-        amountOfAdults = scan.nextInt();
+        Simulation.amountOfAdults = scan.nextInt();
         System.out.println("Podaj ilość dzieci: ");
-        amountOfChildren = scan.nextInt();
+        Simulation.amountOfChildren = scan.nextInt();
         System.out.println("Podaj ilość tarzanów: ");
-        amountOfTarzans = scan.nextInt();
+        Simulation.amountOfTarzans = scan.nextInt();
         System.out.println("Podaj ilość kryjówek: ");
-        numberOfHidings = scan.nextInt();
+        Simulation.numberOfHidings = scan.nextInt();
         scan.close();
+
+
 
     }
 }
--- src/Simulation.java
+++ src/Simulation.java
@@ -1,7 +1,25 @@
 public class Simulation {
 
-    public static int mapSize;
-    String[][] map = new String[mapSize][mapSize];
+    static int boardSize;
+    static int amountOfMonkeys;
+    static int amountOfLions;
+    static int amountOfAdults;
+    static int amountOfChildren;
+    static int amountOfTarzans;
+    static int numberOfHidings;
+
+    String[][] board1 = new String[boardSize][boardSize];
+
+     public void boardDisplay() {
+        for (int i = 0; i < boardSize; i++) {
+            for (int j = 0; j < boardSize; j++) {
+                System.out.print("[" + board1[i][j] + "]");
+            }
+            System.out.println();
+        }
+    }
+    
+
 
 
 }
