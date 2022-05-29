58e29f583ca6623eba27394eb8dcd2dab4721d3d
aleksandrachrustek <106326730+aleksandrachrustek@users.noreply.github.com>
2022-05-30 00:05:01 +0200

    x

--- src/Simulation.java
+++ src/Simulation.java
@@ -1,25 +1,6 @@
 public class Simulation {
 
-    static int boardSize;
-    static int amountOfMonkeys;
-    static int amountOfLions;
-    static int amountOfAdults;
-    static int amountOfChildren;
-    static int amountOfTarzans;
-    static int numberOfHidings;
 
-    //tworzenie tablicy dwuwymiarowej - planszy
-     String[][] board1 = new String[boardSize][boardSize];
-
-    //wyświetlanie planszy
-    public void boardDisplay() {
-        for (int i = 0; i < boardSize; i++) {
-            for (int j = 0; j < boardSize; j++) {
-                System.out.print("[" + board1[i][j] + "]");
-            }
-            System.out.println();
-        }
-    }
 
 
 }
