b9fa209835de42336039c1c65307b16daa90d335
aleksandrachrustek <106326730+aleksandrachrustek@users.noreply.github.com>
2022-05-29 18:59:12 +0200

    go method

--- src/Agent.java
+++ src/Agent.java
@@ -1,5 +1,57 @@
+import java.util.Random;
 abstract class Agent {
-    void go() {
 
+    int x;
+    int y;
+    Agent(int x, int y){
+        this.x = x;
+        this.y = y;
     }
+    void go(String[][] board1, int boardSize) {
+        Random r = new Random();
+        int choice = r.nextInt(8) + 1;
+
+        switch (choice) {
+            case 1:
+                if (x < boardSize - 1) {
+                    x++;
+                } else {
+                    go(board1, boardSize);
+                }
+            case 2:
+                if (y < boardSize - 1) {
+                    y++;
+                } else {
+                    go(board1, boardSize);
+                }
+            case 3:
+                if (x < boardSize - 1 && y < boardSize - 1) {
+                    x++;
+                    y++;
+                } else {
+                    go(board1, boardSize);
+                }
+            case 4:
+                if () {
+                    x--;
+                }
+            case 5:
+                if () {
+                    y--;
+                }
+            case 6:
+                if () {
+                    x--;
+                    y--;
+                }
+            case 7:
+                if () {
+                    y++;
+                }
+            case 8:
+                if () {
+                    y++;
+                }
+        }
+
 }
