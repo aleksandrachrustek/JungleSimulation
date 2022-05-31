912f75b53a1c726e7cca4e52a7d31a2e883a2260
aleksandrachrustek <106326730+aleksandrachrustek@users.noreply.github.com>
2022-05-31 19:23:45 +0200

    hiding

--- src/Hiding.java
+++ src/Hiding.java
@@ -1,10 +1,18 @@
+import java.util.Random;
+
 public class Hiding {
 
+    public static int x;
+    public static int y;
+
     public Hiding(int x, int y){
-
+        this.x=x;
+        this.y=y;
+    }
+    public static void choosePlaces(int size) {
+        Random rand = new Random();
+        x = rand.nextInt(size);
+        y = rand.nextInt(size);
     }
 
-    public void choosePlaces() {
-
-    }
 }
