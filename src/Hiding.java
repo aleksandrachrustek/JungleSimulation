69b1762d8b137a7c82433a0c916118c9f5e2a713
aleksandrachrustek <106326730+aleksandrachrustek@users.noreply.github.com>
2022-05-31 23:03:34 +0200

    nothing new

--- src/Hiding.java
+++ src/Hiding.java
@@ -1,18 +1,8 @@
 import java.util.Random;
 
-public class Hiding {
+public class Hiding extends Agent {
 
-    public static int x;
-    public static int y;
-
-    public Hiding(int x, int y){
-        this.x=x;
-        this.y=y;
+    public Hiding(int x, int y) {
+        super(x,y);
     }
-    public static void choosePlaces(int size) {
-        Random rand = new Random();
-        x = rand.nextInt(size);
-        y = rand.nextInt(size);
-    }
-
 }
