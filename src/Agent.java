cd4302bce37965aaeca768e31f8047f2998656ae
aleksandrachrustek <106326730+aleksandrachrustek@users.noreply.github.com>
2022-05-31 19:23:09 +0200

    movement

--- src/Agent.java
+++ src/Agent.java
@@ -1,14 +1,9 @@
-import java.util.ArrayList;
 import java.util.Random;
 abstract class Agent {
 
     private boolean hidden;
     private int pos[] = new int [2];
 
-    Agent(){
-        this.pos[0]=this.pos[1]=0;
-        this.hidden=false;
-    }
 
     Agent(int x, int y){
         this.pos[0] = x;
@@ -22,84 +17,76 @@ abstract class Agent {
         int dir = rand.nextInt(8);
 
         switch (dir) {
-            case 0: {
-                if(pos[0]==0){
+            case 0 -> {
+                if (pos[0] == 0) {
                     System.out.println("nie mozna isc w gore");
-                }
-                else{
+                } else {
                     System.out.println("Ruch w gore");
                     pos[0]--;
                 }
                 break;
             }
-            case 1: {
-                if(pos[1]==0){
+            case 1 -> {
+                if (pos[1] == 0) {
                     System.out.println("nie mozna isc w lewo");
-                }
-                else{
+                } else {
                     System.out.println("Ruch w lewo");
                     pos[1]--;
                 }
                 break;
             }
-            case 2: {
-                if(pos[0]== map.getSize()-1){
+            case 2 -> {
+                if (pos[0] == map.getSize() - 1) {
                     System.out.println("nie mozna isc w dol");
-                }
-                else{
+                } else {
                     System.out.println("Ruch w dol");
                     pos[0]++;
                 }
                 break;
             }
-            case 3: {
-                if(pos[1]== map.getSize()-1){
+            case 3 -> {
+                if (pos[1] == map.getSize() - 1) {
                     System.out.println("nie mozna isc w prawo");
-                }
-                else{
+                } else {
                     System.out.println("Ruch w prawo");
                     pos[1]++;
                 }
                 break;
             }
-            case 4: {
-                if(pos[0]==0 && pos[1]==0){
+            case 4 -> {
+                if (pos[0] == 0 && pos[1] == 0) {
                     System.out.println("nie mozna wykonac ruchu");
-                }
-                else{
+                } else {
                     System.out.println("Ruch w gore i lewo");
                     pos[0]--;
                     pos[1]--;
                 }
                 break;
             }
-            case 5: {
-                if(pos[0]== map.getSize()-1 && pos[1]== map.getSize()-1){
+            case 5 -> {
+                if (pos[0] == map.getSize() - 1 && pos[1] == map.getSize() - 1) {
                     System.out.println("nie mozna wykonac ruchu");
-                }
-                else{
+                } else {
                     System.out.println("Ruch w dol i prawo");
                     pos[0]++;
                     pos[1]++;
                 }
                 break;
             }
-            case 6: {
-                if(pos[1]==0 || pos[0]== map.getSize()-1){
+            case 6 -> {
+                if (pos[1] == 0 || pos[0] == map.getSize() - 1) {
                     System.out.println("nie mozna wykonac ruchu");
-                }
-                else{
+                } else {
                     System.out.println("Ruch w gore i prawo");
                     pos[0]++;
                     pos[1]--;
                 }
                 break;
             }
-            case 7: {
-                if(pos[1]== map.getSize()-1 || pos[0]==0){
+            case 7 -> {
+                if (pos[1] == map.getSize() - 1 || pos[0] == 0) {
                     System.out.println("nie mozna wykonac ruchu");
-                }
-                else{
+                } else {
                     System.out.println("Ruch w dol i lewo");
                     pos[0]--;
                     pos[1]++;
