3d31686a2b312108c354432f5cad0e77c1190195
aleksandrachrustek <106326730+aleksandrachrustek@users.noreply.github.com>
2022-06-06 00:42:29 +0200

    basically nothing

--- src/Adult.java
+++ src/Adult.java
@@ -1,17 +1,19 @@
-//human, adult, child, tarzan, map, i agent
-
 public class Adult extends Human {
-    private int age;
-
+    private int age=18;
     public Adult(int x, int y){
         super(x,y);
     }
-
     @Override
     public void go(Simulation map) {
         map.setCharacter(getPosition()[0], getPosition()[1], "[ ]");
         super.go(map);
         map.setCharacter(getPosition()[0], getPosition()[1], "[A]");
+    }
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
     }
 
 }
--- src/Child.java
+++ src/Child.java
@@ -1,15 +1,20 @@
 public class Child extends Human {
-    private int age;
+    private int age=1;
     Child(int x, int y){
         super(x,y);
         this.type = "CHILD";
     }
-
     @Override
     public void go(Simulation map) {
         map.setCharacter(getPosition()[0], getPosition()[1], "[ ]");
         super.go(map);
         map.setCharacter(getPosition()[0], getPosition()[1], "[C]");
+    }
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
     }
 
 }
