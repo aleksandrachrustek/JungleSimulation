a78c0721e1aea1df0e0615136194e8558f3c5d9c
aleksandrachrustek <106326730+aleksandrachrustek@users.noreply.github.com>
2022-06-06 10:42:28 +0200

    added interactions

--- src/Map.java
+++ src/Map.java
@@ -4,40 +4,48 @@ public class Map extends Simulation {
 
     public static ArrayList<Agent> agents = new ArrayList<>();
 
-    //interakcje miedzy agentami (dodac smierc czlowieka ze wzgledu na wiek, zamiane dziecka w doroslego, kryjowke)
+    //interakcje miedzy agentami
     public void interactions() {
 
         for (Agent agent1 : Map.agents) {
             String type1 = agent1.getType();
-            /*if (type1.equals("ADULT") || type1.equals("CHILD")) {
-                //inkrementacja wieku
-
-            }*/
+            //operacje na wieku
+            if (type1.equals("ADULT") || type1.equals("CHILD")) {
+                int a = agent1.getAge();
+                agent1.setAge(a+1);
+            }
+            if (type1.equals("ADULT")) {
+                int a = agent1.getAge();
+                if(a>=99) {
+                    setCharacter(agent1.getPosition()[0], agent1.getPosition()[1], "[ ]");
+                    Map.agents.remove(agent1);
+                }
+            }
+            if (type1.equals("CHILD")) {
+                int a = agent1.getAge();
+                if(a>=18) {
+                    setCharacter(agent1.getPosition()[0], agent1.getPosition()[1], "[A]");
+                    Map.agents.add(new Adult(agent1.getPosition()[0],agent1.getPosition()[1]));
+                    Map.agents.remove(agent1);
+                }
+            }
             for (Agent agent2 : Map.agents) {
                 if (agent1 != agent2) {
                     String type2 = agent2.getType();
                     if (agent1.getPosition() == agent2.getPosition()) {
+                        //kryjowka
+                        if ((type1.equals("ADULT")||type1.equals("CHILD")||type1.equals("MONKEY")) && type2.equals("HIDING")) {
+                            setCharacter(agent2.getPosition()[0], agent2.getPosition()[1], "[X]");
+                            for (Agent agent3 : Map.agents) {
+                                if (agent3.getType().equals("LION")) {
+                                    agent3.go(this);
+                                }
+                            }
+                        }
+                        //funkcja kill
                         if (type1.equals("MONKEY") && type2.equals("LION")) {
                             setCharacter(agent1.getPosition()[0], agent1.getPosition()[1], "[L]");
                             Map.agents.remove(agent1);
-                        }
-                        if (type1.equals("MONKEY") && type2.equals("ADULT")) {
-                            findFreeSpace();
-                            setCharacter(x, y, "[M]");
-                            agent1.setPosition(x, y);
-                            setCharacter(agent2.getPosition()[0], agent2.getPosition()[1], "[A]");
-                        }
-                        if (type1.equals("MONKEY") && type2.equals("CHILD")) {
-                            findFreeSpace();
-                            setCharacter(x, y, "[M]");
-                            agent1.setPosition(x, y);
-                            setCharacter(agent2.getPosition()[0], agent2.getPosition()[1], "[C]");
-                        }
-                        if (type1.equals("MONKEY") && type2.equals("TARZAN")) {
-                            findFreeSpace();
-                            setCharacter(x, y, "[M]");
-                            agent1.setPosition(x, y);
-                            setCharacter(agent2.getPosition()[0], agent2.getPosition()[1], "[T]");
                         }
                         if (type1.equals("LION") && type2.equals("ADULT")) {
                             setCharacter(agent1.getPosition()[0], agent1.getPosition()[1], "[L]");
@@ -51,29 +59,61 @@ public class Map extends Simulation {
                             setCharacter(agent2.getPosition()[0], agent2.getPosition()[1], "[T]");
                             Map.agents.remove(agent1);
                         }
+                        //ponowny ruch agentow nie zabijajacych sie nawzajem
+                        if (type1.equals("MONKEY") && type2.equals("ADULT")) {
+                            setCharacter(agent1.getPosition()[0], agent1.getPosition()[1], "[A]");
+                            agent2.setPosition(agent1.getPosition()[0], agent1.getPosition()[1]);
+                            agent1.go(this);
+                        }
+                        if (type1.equals("MONKEY") && type2.equals("CHILD")) {
+                            setCharacter(agent1.getPosition()[0], agent1.getPosition()[1], "[C]");
+                            agent2.setPosition(agent1.getPosition()[0],agent1.getPosition()[1]);
+                            agent1.go(this);
+                        }
+                        if (type1.equals("MONKEY") && type2.equals("TARZAN")) {
+                            agent1.go(this);
+                            agent2.go(this);
+                        }
+                        if (type1.equals("ADULT") && type2.equals("CHILD")) {
+                                setCharacter(agent1.getPosition()[0], agent1.getPosition()[1], "[C]");
+                                agent2.setPosition(agent1.getPosition()[0], agent1.getPosition()[1]);
+                                agent1.go(this);
+                        }
+                        //tarzan pomaga ludziom
                         if (type1.equals("ADULT") && type2.equals("TARZAN")) {
                             setCharacter(agent2.getPosition()[0], agent2.getPosition()[1], "[T]");
                             for (Agent agent3 : Map.agents) {
                                 if (agent3.getType().equals("MONKEY")) {
-                                    findFreeSpace(agent3.getPosition()[0], agent3.getPosition()[1]);
-                                    agent1.setPosition(x, y);
-                                    setCharacter(x, y, "[A]");
+                                    agent1.setPosition(agent3.getPosition()[0], agent3.getPosition()[1]);
+                                    setCharacter(agent3.getPosition()[0], agent3.getPosition()[1], "[A]");
+                                    agent3.go(this);
                                 }
                             }
                         }
+                        if (type1.equals("CHILD") && type2.equals("TARZAN")) {
+                            setCharacter(agent2.getPosition()[0], agent2.getPosition()[1], "[T]");
+                            for (Agent agent3 : Map.agents) {
+                                if (agent3.getType().equals("MONKEY")) {
+                                    agent1.setPosition(agent3.getPosition()[0], agent3.getPosition()[1]);
+                                    setCharacter(agent3.getPosition()[0], agent3.getPosition()[1], "[C]");
+                                    agent3.go(this);
+                                }
+                            }
+                        }
+                        //funkcja copy
                         if (type1.equals("LION") && type2.equals("LION")) {
                             findFreeSpace();
-                            Map.agents.add(new Lion(x, y));
+                            Map.agents.add(new Lion(x,y));
                             this.map[x][y] = "[L]";
                         }
                         if (type1.equals("MONKEY") && type2.equals("MONKEY")) {
                             findFreeSpace();
-                            Map.agents.add(new Monkey(x, y));
+                            Map.agents.add(new Monkey(x,y));
                             this.map[x][y] = "[M]";
                         }
                         if (type1.equals("ADULT") && type2.equals("ADULT")) {
                             findFreeSpace();
-                            Map.agents.add(new Child(x, y));
+                            Map.agents.add(new Child(x,y));
                             this.map[x][y] = "[C]";
                         }
                     }
