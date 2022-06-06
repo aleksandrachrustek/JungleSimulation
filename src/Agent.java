22ab49b582792fce9bfb5fb99bfeae4def01f69a
aleksandrachrustek <106326730+aleksandrachrustek@users.noreply.github.com>
2022-06-06 14:44:36 +0200

    movement changes

--- src/Agent.java
+++ src/Agent.java
@@ -1,11 +1,12 @@
 import java.util.Random;
+
 abstract class Agent {
     private int age;
     protected String type;
     private int[] pos = new int[2];
 
 
-    Agent(int x, int y) {
+    Agent(int x, int y, int age) {
         this.pos[0] = x;
         this.pos[1] = y;
         this.age = age;
@@ -14,81 +15,58 @@ abstract class Agent {
     //poruszanie sie agentow
     public void go(Simulation map) {
         Random rand = new Random();
-        int dir = rand.nextInt(8);
+        int choice = rand.nextInt(8);
 
-        int a = 0;
         do {
-            switch (dir) {
-                case 0 -> {
-                    if (pos[0] == 0) {
-                        break;
-                    } else {
+            switch (choice) {
+                case 0: {
+                    if (!(pos[0] == 0)) {
                         pos[0]--;
-                        a = 1;
                     }
                 }
-                case 1 -> {
-                    if (pos[1] == 0) {
-                        break;
-                    } else {
+                case 1: {
+                    if (!(pos[1] == 0)) {
                         pos[1]--;
-                        a = 1;
                     }
                 }
-                case 2 -> {
-                    if (pos[0] == map.getSize() - 1) {
-                        break;
-                    } else {
+                case 2: {
+                    if (!(pos[0] == map.getSize() - 1)) {
                         pos[0]++;
-                        a = 1;
                     }
                 }
-                case 3 -> {
-                    if (pos[1] == map.getSize() - 1) {
-                        break;
-                    } else {
+                case 3: {
+                    if (!(pos[1] == map.getSize() - 1)) {
                         pos[1]++;
-                        a = 1;
                     }
                 }
-                case 4 -> {
-                    if (pos[0] == 0 || pos[1] == 0) {
-                        break;
-                    } else {
+                case 4: {
+                    if (!(pos[0] == 0 || pos[1] == 0)) {
                         pos[0]--;
                         pos[1]--;
-                        a = 1;
                     }
                 }
-                case 5 -> {
-                    if (pos[0] == map.getSize() - 1 || pos[1] == map.getSize() - 1) {
-                        break;
-                    } else {
+                case 5: {
+                    if (!(pos[0] == map.getSize() - 1 || pos[1] == map.getSize() - 1)) {
                         pos[0]++;
                         pos[1]++;
-                        a = 1;
                     }
                 }
-                case 6 -> {
-                    if (pos[1] == 0 || pos[0] == map.getSize() - 1) {
-                        break;
-                    } else {
+                case 6: {
+                    if (!(pos[1] == 0 || pos[0] == map.getSize() - 1)) {
                         pos[0]++;
                         pos[1]--;
-                        a = 1;
                     }
                 }
-                case 7 -> {
-                    if (pos[1] == map.getSize() - 1 || pos[0] == 0) {
-                        break;
-                    } else {
+                case 7: {
+                    if (!(pos[1] == map.getSize() - 1 || pos[0] == 0)) {
                         pos[0]--;
                         pos[1]++;
-                        a = 1;
                     }
                 }
+                default:
+                    choice = -1;
             }
-        } while (a == 0);
+        } while (choice != -1);
     }
 
     public int[] getPosition() {
